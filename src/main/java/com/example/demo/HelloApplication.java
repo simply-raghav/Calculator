dc7d4af0e5b58d35958efd7f34c917787870a94e
Yogesh Patel <yogesh.2022ca115@mnnit.ac.in>
2022-11-07 00:34:34 +0530

    Improvised UI 1.1

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main-Window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 290, 348);
+        Scene scene = new Scene(fxmlLoader.load(), 363, 424);
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
